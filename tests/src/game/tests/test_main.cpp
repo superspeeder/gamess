31ee0422292744a5fe7bc244b1459b08cb51443e
superspeeder <andy@delusion.org>
2025-06-30 14:07:47 -0700

    start adding tests

---
+++ tests/src/game/tests/test_main.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <game/render/buffer.hpp>
+
+/**
+ * TODO: implement tests
+ * I need to manage the fact that most of my operations hinge on OpenGL state, which I need to both mock so I can test that things *would* work in valid opengl contexts, and also I need a valid opengl context for some tests (to ensure that I am properly using OpenGL).
+ *
+ * There are several things I can implement tests for without this though. For example, I can test the lru cache
+ */
